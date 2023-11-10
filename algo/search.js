ef6304ca1353d499656ff10d9457d79f68b8e0c2
Aralhi <xiaoping0501@gmail.com>
2023-11-10 19:29:26 +0800

    fix: 修改注释

--- algo/search.js
+++ algo/search.js
@@ -1,10 +1,8 @@
 /**
- * 整数数组 nums 按升序排列，数组中的值 互不相同 。
-
+ * 
+整数数组 nums 按升序排列，数组中的值 互不相同 。
 在传递给函数之前，nums 在预先未知的某个下标 k（0 <= k < nums.length）上进行了 旋转，使数组变为 [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]]（下标 从 0 开始 计数）。例如， [0,1,2,4,5,6,7] 在下标 3 处经旋转后可能变为 [4,5,6,7,0,1,2] 。
-
 给你 旋转后 的数组 nums 和一个整数 target ，如果 nums 中存在这个目标值 target ，则返回它的下标，否则返回 -1 。
-
 你必须设计一个时间复杂度为 O(log n) 的算法解决此问题。
 
 示例 1：
@@ -42,17 +40,18 @@ var search = function(nums, target) {
       if (nums[mid] === target) {
           return mid
       }
-      if (nums[0] <= nums[mid]) {
-          // 有序
-          if (nums[0] <= target && target < nums[r]) {
-              // 如果target在有序的区间里，则搜索该区间
+      if (nums[l] <= nums[mid]) {
+          // 左侧有序
+          if (nums[l] <= target && target <= nums[mid]) {
+              // 如果target在有序的区间里，则搜索左侧区间
               r = mid - 1
           } else {
+              // 否则搜索右侧区间
               l = mid + 1
           }
       } else {
-          // 无序
-          if (nums[mid] < target && target <= nums[n - 1]) {
+          // 右侧有序
+          if (nums[mid] <= target && target <= nums[r]) {
               l = mid + 1
           } else {
               r = mid - 1
@@ -61,3 +60,10 @@ var search = function(nums, target) {
   }
   return -1
 };
+
+/**
+ * 搞懂这个问题的精髓在于三个定理
+ * 1. 只有在有序区间才可以通过区间的两个端点判断target是否在其中。
+ * 2. 判断有序区间还是无序区间，通过判断左右两个端点的值即可
+ * 3. 每次二分至少会出现一个有序区间。
+ */
